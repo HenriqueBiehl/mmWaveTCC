e567251b5391a5f21bb0d9aab6746f7079d1cd86
gustavops <guprado.silva@gmail.com>
2026-04-29 21:46:23 -0300

    [fix] gene_size variavel

--- Genetic/main.py
+++ Genetic/main.py
@@ -2,11 +2,10 @@ import numpy as np
 import sys
 import genetic_scheduling as gs
 
-gene_size = 2
-initial_population = 500
-num_generations = 100
+population_size = 5
+num_generations = 50
 elitism_rate = 0.2
-tournament_size = 2
+tournament_size = 3
 mutation_rate = 0.2
 
 # Lê toda a entrada do arquivo ou stdin  
@@ -39,9 +38,9 @@ for k in range(0, gene_size):
             predicted_rate[i][j] = dados[idx] 
             idx += 1
     
-    print(f'Predicted rate for session {k}:')
-    print(predicted_rate)
-    print("") 
+    # print(f'Predicted rate for session {k}:')
+    # print(predicted_rate)
+    # print("") 
 
     scheduling_sesssions_list.append(predicted_rate)
 
@@ -49,14 +48,14 @@ scheduling_sesssions = np.array(scheduling_sesssions_list)
 
 print("Scheduling session aggregation:")
 print(scheduling_sesssions)
-print("") 
+print("")
 
 
-population = gs.initial_population_random(scheduling_sesssions.copy(), user_nts_constraint.copy(), gene_size, initial_population, nts, nu)
+population = gs.initial_population_random(scheduling_sesssions.copy(), user_nts_constraint.copy(), gene_size, population_size, nts, nu)
 population_copy = population.copy()
 
 # print("Initial Population:")
-# gs.print_population(population, initial_population, gene_size,  nts)
+# gs.print_population(population, population_size, gene_size,  nts)
 # print("")
 
 print("Crossover using Roulette and Session Swap Mutation")
@@ -64,16 +63,16 @@ print("\n")
 max_fit = 0.0
 max_user = []
 for i in range(num_generations):
-    new_population = gs.crossover(population, elitism_rate, tournament_size,  gene_size, initial_population, nts, "roulette")
-    new_population = gs.session_mutation(new_population, scheduling_sesssions, mutation_rate, gene_size, initial_population, nts)
+    new_population = gs.crossover(population, elitism_rate, tournament_size,  gene_size, population_size, nts, "roulette")
+    new_population = gs.session_mutation(new_population, scheduling_sesssions, mutation_rate, gene_size, population_size, nts)
 
     population = new_population.copy()
 
 # print("Final Population:")
-# gs.print_population(new_population, initial_population, gene_size,  nts)
+# gs.print_population(new_population, population_size, gene_size,  nts)
 # print("")
 
-for j in range(initial_population):
+for j in range(population_size):
     f = gs.fitness(population[j])
     if f > max_fit:
         max_fit = f
